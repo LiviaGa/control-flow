f047d6a9c39fb237d10f882a6f84a1263420b1c8
LiviaGa <liviagallafrio1@gmail.com>
2025-04-16 21:09:22 -0300

    Aprendendo diretivas

---
+++ src/app/components/propertie-binding/propertie-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propertie-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './propertie-binding.component.html',
+  styleUrl: './propertie-binding.component.css'
+})
+export class PropertieBindingComponent {
+imgUrl= "../../../assets/images.jpg"
+isDisabled = true
+descImg = "Imagem the last of us ellie e joel"
+}
