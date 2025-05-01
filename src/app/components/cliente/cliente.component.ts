ce4774c4670c892ea05c4e6b0c74b8bf137d012c
LiviaGa <liviagallafrio1@gmail.com>
2025-04-30 21:36:34 -0300

    Configurando o botão de editar e de remover usuário

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -1,45 +1,108 @@
-import { Component } from '@angular/core';
-import {Cliente} from '../../interfaces/Clientes'
-import {ClienteService} from '../../services/cliente.service'
-import { ReactiveFormsModule, FormBuilder, Validators,FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common'; //importar o módulo Comm
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Cliente } from '../../interfaces/Clientes';
+import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
-clienteForm: FormGroup = new FormGroup({})
-cliente:Cliente[] = []
+  clienteForm: FormGroup = new FormGroup({})
+  cliente: Cliente[] = []
+  clienteIdEdicao: string | null = null
 
-constructor(private clienteService:ClienteService,
-  private formBuilder:FormBuilder)
-  {
-this.clienteForm = formBuilder.group({
-  nome: ['',Validators.required],
-  telefone: ['']
-})
 
-}
+  constructor(private clienteService: ClienteService,
+    private formBuilder: FormBuilder) {
+    this.clienteForm = formBuilder.group({
+      nome: ['', Validators.required],
+      telefone: ['']
+    })
 
-list():void{
-  this.cliente = this.clienteService.list()
-}
-
-//método executado ao inicializar a página
-ngOnInit():void{
-this.list();
-}
-
-save():void{
-  if(this.clienteForm.valid){
-    alert('Podemos salvar!')
-  }else{
-    alert('Formulário incorreto')
   }
+
+  list(): void {
+    this.cliente = this.clienteService.list()
+  }
+
+  //método executado ao inicializar a página
+  ngOnInit(): void {
+    this.list();
+  }
+
+  generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+
+  save() {
+    if (this.clienteForm.valid) {
+      const formData = this.clienteForm.value
+
+      if (this.clienteIdEdicao) {
+        const clienteUpdate: Cliente = {
+          id: this.clienteIdEdicao,
+          nome: formData.nome,
+          telefone: formData.telefone
+        }
+        this.clienteService.update(this.clienteIdEdicao, clienteUpdate)
+        this.clienteIdEdicao = null
+        alert('Alterado com sucesso!')
+      } else {
+        const clienteAdd: Cliente = {
+          id: this.generateRandomString(6),
+          nome: formData.nome,
+          telefone: formData.telefone
+        }
+        //console.log(clienteAdd)
+        this.clienteService.add(clienteAdd) //chamando a service para inserir
+        alert('Inserido com sucesso') //enviando feedback ao usuário
+      }
+      //this.list() //chamar service e recarrega com o item inserido
+    } else {
+      alert('Por Favor preencher os campos obrigatórios!')
+    }
+
+    this.clienteForm.reset() //limpar o form após o preenchimento
+
+    //if(this.clienteForm.valid){
+    //  alert('Podemos salvar!')
+    //}else{
+    //  alert('Formulário incorreto')
+    //}
+  }
+
+  editar(id: string): void {
+    //buscando todos os clientes e filtrnado pelo id enviado como parametro
+    const cliente = this.clienteService.list().find(c => c.id == id)
+    if (cliente) {
+      this.clienteIdEdicao = cliente.id
+      //atribuir os valores ao formulário
+      this.clienteForm.patchValue(
+        {
+          nome: cliente.nome,
+          telefone: cliente.telefone
+        }
+      )
+    }
+    //console.log(cliente)
+  }
+
+remover(id:string):void{
+this.clienteService.remove(id)
 }
+
+
 
 }
